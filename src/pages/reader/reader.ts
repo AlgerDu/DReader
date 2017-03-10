937a04b79732b51288646c0332a40fd6e246178b
Alger <AlgerDu@outlook.com>
2017-03-10 22:14:11 +0800

    当阅读器工具栏打开时，后退按钮关闭阅读器工具栏

--- src/pages/reader/reader.ts
+++ src/pages/reader/reader.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { MenuController } from 'ionic-angular';
+import { Platform } from 'ionic-angular';
 
 /*
   Generated class for the Reader page.
@@ -18,7 +19,14 @@ export class ReaderPage {
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    public menuCtrl: MenuController) { }
+    public menuCtrl: MenuController,
+    public plt: Platform) {
+
+    this.plt.ready().then(() => {
+      this.registerBackButtonAction();//注册返回按键事件
+    });
+
+  }
 
   ionViewDidLoad() {
     let elements = document.querySelectorAll(".tabbar");
@@ -39,7 +47,19 @@ export class ReaderPage {
     }
   }
 
+  //toggle 阅读器的工具栏
   toggleReaderTool() {
     this.readerToolShow = !this.readerToolShow;
+  }
+
+  //注册页面的 后退按钮事件
+  registerBackButtonAction() {
+    this.plt.registerBackButtonAction(() => {
+      if (this.readerToolShow) {
+        this.readerToolShow = !this.readerToolShow;
+      } else {
+        this.navCtrl.pop();
+      }
+    }, 1);
   }
 }
