76c82b87176112344cad264d3bf67fc674599907
Alger <AlgerDu@outlook.com>
2017-03-16 22:15:54 +0800

    xx做的界面

--- src/app/service/book.service.ts
+++ src/app/service/book.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Headers, Http } from '@angular/http';
+//import { Headers, Http } from '@angular/http';
 
 import { Book, Chapter } from '../model';
 import { AlertController, Platform } from 'ionic-angular';
