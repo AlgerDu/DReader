bcc1367ac62a6c0da547db7a4c4ce7e20c9507b4
Alger <AlgerDu@outlook.com>
2017-03-17 23:52:59 +0800

    获取当前账户对应的书架信息

--- src/app/service/book.service.ts
+++ src/app/service/book.service.ts
@@ -4,6 +4,7 @@ import { Injectable } from '@angular/core';
 import { Book, Chapter } from '../model';
 import { AlertController, Platform } from 'ionic-angular';
 import { SQLiteDbService } from './sqlitedb.service';
+import { AccountService } from './account.service';
 
 @Injectable()
 export class BookService {
@@ -11,26 +12,34 @@ export class BookService {
     constructor(
         private plt: Platform,
         public alertCtrl: AlertController,
-        private dbService: SQLiteDbService
-    ) {
-    }
-    //获取账户正在阅读的书籍，本地和远程书籍
-    SheetList(): Promise<Book[]> {
+        private dbService: SQLiteDbService,
+        private accountService: AccountService
+    ) { }
 
+
+    /**
+     * 获取本地当前账户的书架信息
+     * @returns {Promise<Book[]>} 
+     * 
+     * @memberOf BookService
+     */
+    public SheetList(): Promise<Book[]> {
         if (this.plt.is('core')) {
             let books = [
                 { uid: 'b1', name: '修真聊天群', author: '圣骑士的传说', readPct: 90, updateCount: 3 },
                 { uid: 'b2', name: '神级英雄', author: '', readPct: 0, updateCount: 0 }
             ];
             return Promise.resolve(books);
-        } else {
+        }
+
+        return this.accountService.CurrAccount().then<Book[]>((account) => {
             return this.dbService.executeSql(
                 'select * from BookShelf as bs, Book as b where accUid = ? and bs.bookUid = b.uid',
-                ['a1']
+                [account.uid]
             ).then((data) => {
                 return data as Book[];
             });
-        }
+        });
     }
 
     //获取一本书的目录信息
