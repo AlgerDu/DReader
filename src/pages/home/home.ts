48843fa475751c5ed15172b7d3022fad9a357a83
Alger <AlgerDu@outlook.com>
2017-03-19 22:08:01 +0800

    homepage 代码优化

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -15,7 +15,7 @@ import { AccountService } from '../../app/service/account.service';
 export class HomePage {
   @ViewChild(Content) content: Content;
 
-  private hasLoaded: boolean = false;
+  private hasAutoUpdateBookInfo: boolean = false;
 
   public books: Book[] = [];
   public account: AccountInfo;
@@ -27,30 +27,7 @@ export class HomePage {
     private bookService: BookService,
     private accountService: AccountService
   ) {
-
-    this.events.subscribe(EventType.Account_Loadend.toString(), (time) => {
-      console.log('homepage 处理 Account_Loadend 事件');
-
-      this.account = this.accountService.CurrAccount();
-
-      this.bookService.SheetList().then((books) => {
-        this.books = books;
-
-        if (this.books.length > 0 && this.account.config.autoUpdateBookInfo) {
-          console.log('自动获取小说更新信息');
-          let load = this.loadingCtrl.create({
-            content: '正在刷新...',
-            dismissOnPageChange: true,
-          });
-          load.present();
-          this.UpdateBookInfo().then(() => {
-            load.dismiss();
-          }).catch(() => {
-            load.dismiss();
-          });
-        }
-      });
-    });
+    this.SubscribeAccountLoadendEvent();
   }
 
   ReadBook(book: Book) {
@@ -68,6 +45,13 @@ export class HomePage {
     return this.bookService.Refresh();
   }
 
+  /**
+   * 处理下来刷新
+   * @private
+   * @param {any} refresher 
+   * 
+   * @memberOf HomePage
+   */
   private RefreshBookInfo(refresher) {
     console.log('下拉刷新启动');
     this.UpdateBookInfo().then(() => {
@@ -75,5 +59,41 @@ export class HomePage {
     }).catch(() => {
       refresher.complete();
     })
+  }
+
+  /**
+   * 订阅并且处理 Account_Loadend 事件
+   * 
+   * @private
+   * 
+   * @memberOf HomePage
+   */
+  private SubscribeAccountLoadendEvent() {
+
+    this.events.subscribe(EventType.Account_Loadend.toString(), (time) => {
+      console.log('homepage 处理 Account_Loadend 事件');
+
+      this.account = this.accountService.CurrAccount();
+
+      this.bookService.SheetList().then((books) => {
+        this.books = books;
+
+        if (this.books.length > 0
+          && !this.hasAutoUpdateBookInfo
+          && this.account.config.autoUpdateBookInfo
+        ) {
+          console.log('自动获取小说更新信息');
+          let load = this.loadingCtrl.create({
+            content: '正在刷新...',
+            dismissOnPageChange: true,
+          });
+          load.present();
+          this.UpdateBookInfo().then(() => {
+            load.dismiss();
+            this.hasAutoUpdateBookInfo = true;
+          });
+        }
+      });
+    });
   }
 }
