a5e7e9c6f8c985853e4d8211ea7967df636eb3f6
Alger <AlgerDu@outlook.com>
2017-04-15 13:55:07 +0800

    通用结果返回

---
+++ src/app/models/results.ts
@@ -0,0 +1,36 @@
+export class Result<T>{
+
+}
+
+
+/**
+ * 查询条件
+ * @export
+ * @class SerachCondition
+ */
+export class SerachCondition {
+    constructor(
+        public pageSize: number
+        , public pageIndex: number
+    ) { }
+}
+
+
+/**
+ * 查询结果
+ * @export
+ * @class SearchResult
+ * @template T 
+ */
+export class SearchResult<T>{
+    code: number;
+    pageSize: number;
+    pageIndex: number;
+    recordCount: number;
+    message: string;
+    Data: T[];
+
+    success(): boolean {
+        return this.code == 0;
+    }
+}
