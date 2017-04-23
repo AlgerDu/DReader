1ba739a398c53f033e16a0e4c65cfba4fde98718
Alger <AlgerDu@outlook.com>
2017-04-23 23:15:59 +0800

    返回泛型类完善

--- src/app/models/results.ts
+++ src/app/models/results.ts
@@ -1,5 +1,11 @@
 export class Result<T>{
+    code: number;
+    message: string;
+    data: T;
 
+    public Success(): boolean {
+        return this.code == 0;
+    }
 }
 
 export class BathOpsResultItem<T>{
